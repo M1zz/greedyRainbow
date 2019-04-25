7e69d619944fe35c46a35370295d18fb42745f73
Hyunho Lee <mizzking75@gmail.com>
2019-04-25 23:41:49 +0900

    add testcases

--- schedule/scheduler/controller.py
+++ schedule/scheduler/controller.py
@@ -1,15 +1,26 @@
+import datetime as dt
+
+
 class Controller():
 
-    def is_insert_able(self, user_data, insert_data):
-        start_time = insert_data[0]
-        end_time = insert_data[0]
+    def get_time_length(self, single_task):
+        start = dt.datetime.strptime(single_task[0], "%Y-%m-%d %H:%M")
+        end = dt.datetime.strptime(single_task[1], "%Y-%m-%d %H:%M")
 
-        schedule_data = user_data[2:]
+        time_length = (end - start).seconds / 60
+
+        return time_length
+
+    def is_task_insert_able(self, task_data, insert_data):
+        start_time = insert_data[0]
+        end_time = insert_data[1]
+
+        schedule_data = task_data[2:]
 
         for task in schedule_data:
             reserved_start_time = task[0]
             reserved_end_time = task[1]
-
+            print(start_time, end_time, reserved_start_time, reserved_end_time)
             # 넣는 시작시간 보다 종료시간이 늦으면 겹침
             if reserved_start_time < start_time:
                 if start_time < reserved_end_time:
@@ -24,12 +35,106 @@ class Controller():
 
     def insert(self, user_data, insert_data):
 
-        info = user_data[:2]
-        if self.is_insert_able(user_data, insert_data):
+        if user_data == []:
+            username = "hyunho"
+            user_data.append(username)
+            user_data.append(0)
             user_data.append(insert_data)
-        else:
-            info[1] = int(info[1]) + 1
-            info.append(insert_data)
-            return info
+            return [user_data]
+
+        # TODO 삽입될 때 마다 길이를 잰다
+        # TODO RULE1 가장 긴 녀석 부터 가장 아래에 배치한다.
+        # TODO RULE2 줄에 들어갈 수 있는지 검사해야한다.
+
+        task_bag = []
+        task_bag.append((insert_data, self.get_time_length(insert_data)))
+
+        for line in user_data:
+            items = line[2:]
+            for item in items:
+                task_bag.append((item, self.get_time_length(item)))
+
+        task_bag = self.sort_task(task_bag)
+
+        print("task_bag", task_bag)
+        print("==================")
+
+        username = "hyunho"
+        temp_user_data = []
+        temp_line_data = []
+        line_num = 0
+        for item in task_bag:
+            print("item", item)
+            # 첫번째 삽입
+            if temp_user_data == []:
+                temp_line_data.append(username)
+                temp_line_data.append(line_num)
+                temp_line_data.append(item[0])
+                temp_user_data.append(temp_line_data)
+            else:
+                for line_num in range(0, 7):
+                    # 줄을 생성할 필요가 없다
+                    print("line_info", temp_user_data, len(temp_user_data), line_num)
+                    if len(temp_user_data) > line_num:
+                        temp_line_data = temp_user_data[line_num]
+                        # 현재 줄에 삽입할 수 있으면 삽입한다.
+                        if self.is_task_insert_able(temp_line_data, item[0]):
+                            temp_line_data.append(item[0])
+                            break
+                        else:
+                            temp_line_data = []
+                            print("이번줄에 삽입불가")
+                        # 불가능 하면 다음 줄에 삽입한다.
+                    # 줄을 생성헤서 데이터를 넣어야 한다.
+                    else:
+                        temp_line_data.append(username)
+                        temp_line_data.append(line_num)
+                        temp_line_data.append(item[0])
+                        temp_user_data.append(temp_line_data)
+                        print("들어갔디")
+                        break
+                    print("한바퀴")
+            print("temp_user_data", temp_user_data)
+            print("===================")
+        return temp_user_data
+
+    def update(self, user_data, origin_task, update_task):
+
+        user_data = self.remove_task(user_data, origin_task)
+
+        user_data = self.insert(user_data, update_task)
 
         return user_data
+
+    def remove_task(self, user_data, remove_target_task):
+        for line in user_data:
+            for task in line:
+                if remove_target_task == task:
+                    line.remove(task)
+
+        return user_data
+
+    def sort_task(self, tasks):
+        tasks.sort(key=lambda element: element[1], reverse=True)
+        return tasks
+
+
+"""
+    def sort_task(self,task_data):
+        tasks = []
+        for task in task_data:
+            
+        return task_data
+
+
+    # TODO : AFTER CONNECT DATABASE
+    
+    def select(self, username):
+
+        return user_data
+        
+        
+    def delete(self):
+        return True
+    
+"""
--- schedule/tests/test_rainbow_logic.py
+++ schedule/tests/test_rainbow_logic.py
@@ -6,48 +6,179 @@ user = "hyunho"
 
 class LogicTest(BaseTestCase):
 
-    def test_스케쥴_입력(self):
+    def test_일정길이(self):
+        # Given
+        single_task = ["2019-04-16 09:30", "2019-04-16 10:30"]
+        c = controller.Controller()
+        expected = 60
+
+        # When
+        time_length = c.get_time_length(single_task)
+
+        # Then
+        self.assertEqual(time_length, expected)
+
+    def test_스케쥴_검색(self):
+        # Given
+        # userdata =
+
+        # When
+
+        # Then
+
         self.assertEqual(0, 1)
 
     def test_스케쥴_삽입가능(self):
         # Given
-        user_data = [user, 0, ["2019-04-16 09:30", "2019-04-16 10:30"], ["2019-04-16 13:00", "2019-04-16 15:00"]]
+        user_data = ["hyunho", 0, ["2019-04-16 09:30", "2019-04-16 10:30"], ["2019-04-16 13:00", "2019-04-16 15:00"]]
         insert_data = ["2019-04-16 11:00", "2019-04-16 12:00"]
         expected = True
         c = controller.Controller()
 
         # When
-        result = c.is_insert_able(user_data, insert_data)
+        result = c.is_task_insert_able(user_data, insert_data)
+
+        # Then
+        self.assertEqual(result, expected)
+
+    def test_스케쥴_삽입_불가능(self):
+        # Given
+        user_data = ['hyunho', 0, ['2019-04-16 10:00', '2019-04-16 12:00'], ['2019-04-16 13:00', '2019-04-16 15:00']]
+        insert_data = ['2019-04-16 09:30', '2019-04-16 10:30']
+        expected = False
+        c = controller.Controller()
+
+        # When
+        result = c.is_task_insert_able(user_data, insert_data)
+
+        # Then
+        self.assertEqual(result, expected)
+
+    def test_스케쥴_삽입_처음(self):
+        # Given
+        user_data = []
+        insert_data = ["2019-04-16 11:00", "2019-04-16 12:00"]
+        expected = [["hyunho", 0, ["2019-04-16 11:00", "2019-04-16 12:00"]]]
+        c = controller.Controller()
+
+        # When
+        result = c.insert(user_data, insert_data)
 
         # Then
         self.assertEqual(result, expected)
 
     def test_스케쥴_삽입_1번째_줄(self):
         # Given
-        user_data = [user, 0, ["2019-04-16 09:30", "2019-04-16 10:30"], ["2019-04-16 13:00", "2019-04-16 15:00"]]
+        user_data = [["hyunho", 0, ["2019-04-16 09:30", "2019-04-16 10:30"], ["2019-04-16 13:00", "2019-04-16 15:00"]]]
         insert_data = ["2019-04-16 11:00", "2019-04-16 12:00"]
-        expected = [user, 0, ["2019-04-16 09:30", "2019-04-16 10:30"], ["2019-04-16 13:00", "2019-04-16 15:00"],
-                    ["2019-04-16 11:00", "2019-04-16 12:00"]]
+        expected = [["hyunho", 0, ["2019-04-16 09:30", "2019-04-16 10:30"], ["2019-04-16 13:00", "2019-04-16 15:00"],
+                     ["2019-04-16 11:00", "2019-04-16 12:00"]]]
+        temp = [['hyunho', 0, ['2019-04-16 10:00', '2019-04-16 12:00'], ['2019-04-16 13:00', '2019-04-16 15:00']]]
         c = controller.Controller()
 
         # When
         result = c.insert(user_data, insert_data)
 
         # Then
-        self.assertEqual(result, expected)
+        self.assertCountEqual(result[0], expected[0])
 
-    def test_스케쥴_삽입_다음_줄(self):
+    def test_스케쥴_삽입_2번째_줄(self):
         # Given
-        user_data = [user, 0, ["2019-04-16 09:30", "2019-04-16 10:30"], ["2019-04-16 13:00", "2019-04-16 15:00"]]
+        user_data = [["hyunho", 0, ["2019-04-16 09:30", "2019-04-16 10:30"], ["2019-04-16 13:00", "2019-04-16 15:00"]]]
         insert_data = ["2019-04-16 10:00", "2019-04-16 12:00"]
-        expected = [user, 1, ["2019-04-16 10:00", "2019-04-16 12:00"]]
+        expected = [["hyunho", 0, ["2019-04-16 10:00", "2019-04-16 12:00"], ["2019-04-16 13:00", "2019-04-16 15:00"]],
+                    ["hyunho", 1, ["2019-04-16 09:30", "2019-04-16 10:30"]]]
+        c = controller.Controller()
+
+        # When
+        result = c.insert(user_data, insert_data)
+        # Then
+        self.assertCountEqual(result, expected)
+
+    def test_스케쥴_삽입_긴_줄(self):
+        # Given
+        user_data = [["hyunho", 0, ["2019-04-16 09:30", "2019-04-16 10:30"], ["2019-04-16 13:00", "2019-04-16 15:00"]],
+                     ["hyunho", 1, ["2019-04-16 10:00", "2019-04-16 12:00"]]]
+        insert_data = ["2019-04-16 01:00", "2019-04-16 23:00"]
+        expected = [["hyunho", 0, ["2019-04-16 01:00", "2019-04-16 23:00"]],
+                    ["hyunho", 1, ["2019-04-16 10:00", "2019-04-16 12:00"], ["2019-04-16 13:00", "2019-04-16 15:00"]],
+                    ["hyunho", 2, ["2019-04-16 09:30", "2019-04-16 10:30"]]]
         c = controller.Controller()
 
         # When
         result = c.insert(user_data, insert_data)
 
+        print("expected", expected)
+        print("result", result)
+
+        # Then
+        self.assertCountEqual(result[0], expected[0])
+        self.assertCountEqual(result[1], expected[1])
+        self.assertCountEqual(result[2], expected[2])
+
+    def test_스케쥴_수정_1째줄(self):
+        # Given
+        user_data = [["hyunho", 0, ["2019-04-16 09:30", "2019-04-16 10:30"], ["2019-04-16 13:00", "2019-04-16 15:00"]],
+                     ["hyunho", 1, ["2019-04-16 10:00", "2019-04-16 12:00"]]]
+        origin_data = ["2019-04-16 09:30", "2019-04-16 10:30"]
+        update_data = ["2019-04-16 09:30", "2019-04-16 11:30"]
+        expected = [["hyunho", 0, ["2019-04-16 09:30", "2019-04-16 11:30"], ["2019-04-16 13:00", "2019-04-16 15:00"]],
+                    ["hyunho", 1, ["2019-04-16 10:00", "2019-04-16 12:00"]]]
+        c = controller.Controller()
+
+        # When
+        result = c.update(user_data, origin_data, update_data)
+
+        # Then
+        self.assertCountEqual(result[0], expected[0])
+
+    def test_스케쥴_수정_2째줄로(self):  # Given
+        # Given
+        user_data = [["hyunho", 0, ["2019-04-16 09:30", "2019-04-16 10:30"], ["2019-04-16 13:00", "2019-04-16 15:00"]],
+                     ["hyunho", 1, ["2019-04-16 10:00", "2019-04-16 12:00"]]]
+        origin_task = ["2019-04-16 09:30", "2019-04-16 10:30"]
+        update_task = ["2019-04-16 09:30", "2019-04-16 15:30"]
+        expected = [["hyunho", 0, ["2019-04-16 09:30", "2019-04-16 15:30"]],
+                    ["hyunho", 1, ["2019-04-16 10:00", "2019-04-16 12:00"], ["2019-04-16 13:00", "2019-04-16 15:00"]]]
+        c = controller.Controller()
+
+        # When
+        result = c.update(user_data, origin_task, update_task)
+
+        print(result)
+        print(expected)
+        # Then
+        self.assertCountEqual(result[0], expected[0])
+        self.assertCountEqual(result[1], expected[1])
+
+    def test_일정_삭제(self):
+        # Given
+        user_data = [["hyunho", 0, ["2019-04-16 09:30", "2019-04-16 10:30"], ["2019-04-16 13:00", "2019-04-16 15:00"]],
+                     ["hyunho", 1, ["2019-04-16 10:00", "2019-04-16 12:00"]]]
+        remove_target_task = ["2019-04-16 09:30", "2019-04-16 10:30"]
+        expected = [["hyunho", 0, ["2019-04-16 13:00", "2019-04-16 15:00"]],
+                    ["hyunho", 1, ["2019-04-16 10:00", "2019-04-16 12:00"]]]
+        c = controller.Controller()
+        # When
+        result = c.remove_task(user_data, remove_target_task)
+
         # Then
         self.assertEqual(result, expected)
 
-    def test_스케쥴_수정(self):
+    def test_스케쥴_삭제(self):
         self.assertEqual(0, 1)
+
+
+'''
+    def test_일정_정렬_라인(self):
+        # Given
+        unsorted_task = ['hyunho', 0, ['2019-04-16 13:00', '2019-04-16 15:00'], ['2019-04-16 09:30', '2019-04-16 11:30']]
+        expected = ['hyunho', 0, ['2019-04-16 09:30', '2019-04-16 11:30'],['2019-04-16 13:00', '2019-04-16 15:00']]
+        c = controller.Controller()
+
+        # When
+        result = c.sort_task(unsorted_task)
+
+        # Then
+        self.assertEqual(expected, result)
+'''
